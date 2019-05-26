47183122c153a1117dec627d8f94fbeb3e8f3f5c
IngevanVugt <43170087+IngevanVugt@users.noreply.github.com>
2019-05-26 12:38:06 +0200

    Add files via upload

--- Website/app.py
+++ Website/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template,request
 import mysql.connector
 from Bio.Seq import Seq, transcribe
-import random
 import re
 from Bio.Alphabet import IUPAC
 from Bio.Blast import NCBIWWW, NCBIXML
@@ -28,10 +27,8 @@ def results():
     lijst_kolommen = ["Blast_type", "Score", "Query_cover", "Percent_ident", "E_value", "Accessiecode", "Organisme",
                       "Taxonomie", "Read_type", "Header", "Sequentie", "Prot_naam", "Prot_locatie", "Bio_domein",
                       "Bio_proces", "Bio_functie"]
-    lijst_organisme = ["organisme"]
-    lijst_eiwit = ["eiwit"]
-    lijst_read = ["checkboxread1", "checkboxread2", "checkboxread3"]
-    lijst_blast = ["checkboxblastx", "checkboxtblastx", "checkboxboth"]
+    lijst_organisme = ["organisme", "eiwit", "checkboxread1", "checkboxread2", "checkboxread3",
+                       "checkboxblastx", "checkboxtblastx", "checkboxboth"]
     lijst_getallen = ["E-value_value", "Score_value", "Percent identity", "Query cover"]
     lijst_tekens = ["E_value_parameter", "Score_parameter", "percent_identity_parameter", "query_cover_parameter"]
     resultaat_kolommen = []
@@ -41,31 +38,29 @@ def results():
         result = request.form.get(item)
         if result is not None:
             resultaat_kolommen.append(result)
+
     for item in lijst_organisme:
-        result = request.form.get(item)
-        if len(result) > 0:
-            resultaat_where.append("where Organisme like '%{}%'".format(result))
-    for item in lijst_eiwit:
-        result = request.form.get(item)
-        if len(result) > 0:
-            if len(resultaat_where) > 1:
-                resultaat_where.append("Prot_naam like '%{}%'".format(result))
-            if len(resultaat_where) == 0:
-                resultaat_where.append("where Prot_naam like '%{}%'".format(result))
-    for item in lijst_read:
+        index_getal = lijst_organisme.index(item)
         result = request.form.get(item)
         if result is not None:
-            if len(resultaat_where) > 0:
-                resultaat_where.append("Read_type = {}".format(result))
-            if len(resultaat_where) == 0:
-                resultaat_where.append("where Read_type = {}".format(result))
-    for item in lijst_blast:
-        result = request.form.get(item)
-        if result is not None:
-            if len(resultaat_where) > 0:
-                resultaat_where.append("Blast_type = {}".format(result))
-            if len(resultaat_where) == 0:
-                resultaat_where.append("where Blast_type = {}".format(result))
+            if len(result) > 0:
+                if index_getal == 0:
+                    resultaat_where.append("where Organisme like '%{}%'".format(result))
+                if index_getal == 1:
+                    if len(resultaat_where) > 1:
+                        resultaat_where.append("Prot_naam like '%{}%'".format(result))
+                    elif len(resultaat_where) == 0:
+                        resultaat_where.append("where Prot_naam like '%{}%'".format(result))
+            if index_getal == 2 or index_getal == 3 or index_getal == 4:
+                if len(resultaat_where) > 0:
+                    resultaat_where.append("Read_type = {}".format(result))
+                if len(resultaat_where) == 0:
+                    resultaat_where.append("where Read_type = {}".format(result))
+            if index_getal == 5 or index_getal == 6 or index_getal == 7:
+                if len(resultaat_where) > 0:
+                    resultaat_where.append("Blast_type = {}".format(result))
+                if len(resultaat_where) == 0:
+                    resultaat_where.append("where Blast_type = {}".format(result))
     e_value = ''
     score = ''
     percentidentity = ''
@@ -106,22 +101,12 @@ def results():
     # de query's die worden uitgevoerd
     test = ''
     if len(resultaat_kolommen) == 0:
-        query = "select {} from Seq_Blast join Seq_Read SR on Seq_Blast.Blast_ID = SR.Read_ID " \
-                "join Eiwitten E on Seq_Blast.Blast_ID = E.Prot_ID".format(" , ".join(lijst_kolommen))
-        cursor.execute(query)
-        test = cursor.fetchall()
-    if len(resultaat_kolommen) == 0 and len(resultaat_where) != 0:
         query = "select {} from Seq_Blast join Seq_Read SR on Seq_Blast.Blast_ID = SR.Read_ID join Eiwitten E on " \
                 "Seq_Blast.Blast_ID = E.Prot_ID {}" \
             .format(" , ".join(lijst_kolommen), " and ".join(resultaat_where))
         cursor.execute(query)
         test = cursor.fetchall()
     if len(resultaat_kolommen) != 0:
-        query = "select {} from Seq_Blast join Seq_Read SR on Seq_Blast.Blast_ID = SR.Read_ID " \
-                "join Eiwitten E on Seq_Blast.Blast_ID = E.Prot_ID".format(" , ".join(resultaat_kolommen))
-        cursor.execute(query)
-        test = cursor.fetchall()
-    if len(resultaat_kolommen) != 0 and len(resultaat_where) != 0:
         query = "select {} from Seq_Blast join Seq_Read SR on Seq_Blast.Blast_ID = SR.Read_ID join Eiwitten E on " \
                 "Seq_Blast.Blast_ID = E.Prot_ID {}" \
             .format(" , ".join(resultaat_kolommen), " and ".join(resultaat_where))
@@ -175,10 +160,9 @@ def blast():
         dna = False
         rna = False
         eiwit = False
-        rotzooi = False
+        anders = False
         titel = ''
         resultaat = ''
-
 
         if sequentie is not None:
             if len(re.findall("[ATCG]", sequentie, flags=re.IGNORECASE)) == len(sequentie):
@@ -188,58 +172,74 @@ def blast():
             elif len(re.findall("[ARNDCFQEGHILKMPSTWYV]", sequentie, flags=re.IGNORECASE)) == len(sequentie):
                 eiwit = True
             else:
-                rotzooi = True
+                anders = True
 
         if dna is True:
-            resultaat = "De opgegeven sequentie is DNA"
+            resultaat = "The sequence is DNA"
         elif rna is True:
-            resultaat = "De opgegeven sequentie is RNA"
+            resultaat = "The sequence is RNA"
         elif eiwit is True:
-            resultaat = "De opgegeven sequentie is eiwit"
-        elif rotzooi is True:
-            resultaat = "De opgegeven sequentie is geen DNA, RNA of eiwit sequentie"
+            resultaat = "The sequence is eiwit"
+        elif anders is True:
+            resultaat = "The sequence is not DNA, RNA or protein"
 
-        if sequentie is None:
-            sequentie = ''
+        blast_resultaat = []
         if dna is True:
-            result_handle = NCBIWWW.qblast("tblastx", "nr", str(sequentie))
-            bestand = open("Resultaat_tblastx.xml", "w")
-            resultaat_xml = result_handle.readlines()
-            bestand.writelines(resultaat_xml)
-            bestand.close()
+            sequentie = request.args.get("seq")
+            blast_type = request.args.get("blast_type")
+            if sequentie is not None and blast_type == "blastx":
+                bestand = open("Resultaat.xml", "w")
+                result_handle = NCBIWWW.qblast("blastx", "nr", sequentie)
+                bestand.write(result_handle.getvalue())
+                bestand.close()
 
-            result_handle = open("Resultaat_tblastx.xml", "r")
-            blast_records = NCBIXML.parse(result_handle)
-            blast_record = next(blast_records)
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    titel = alignment.title
-                    titels = titel.split("[")
-                    titelss = titels[1].split("]")
-                    titel = "Blast organisme: " + titelss[0]
-                    break
-            if dna is True and titel == '':
+                result_handle = open("Resultaat.xml", "r")
+                blast_records = NCBIXML.parse(result_handle)
+                blast_record = next(blast_records)
+                for alignment in blast_record.alignments:
+                    for hsp in alignment.hsps:
+                        blast_resultaat.append("****Alignment****")
+                        titel = alignment.title
+                        titels = titel.split("[")
+                        titelss = titels[1].split("]")
+                        titel_ = titel.split("|")
+                        titel__ = titel_[2].split("[")
+                        blast_resultaat.append("Blast organism: {}".format(titelss[0]))
+                        blast_resultaat.append("Protein: {}".format(titel__[0]))
+                        blast_resultaat.append("Sequence: {}".format(alignment.title))
+                        blast_resultaat.append("Length: {}".format(alignment.length))
+                        blast_resultaat.append("E-value: {}".format(hsp.expect))
+                        blast_resultaat.append(hsp.query[0:75] + "...")
+                        blast_resultaat.append(hsp.match[0:75] + "...")
+                        blast_resultaat.append(hsp.sbjct[0:75] + "...")
+                        blast_resultaat.append("\n")
+            if sequentie is not None and blast_type == "tblastx":
+                bestand = open("Resultaat.xml", "w")
+                result_handle = NCBIWWW.qblast("tblastx", "nr", sequentie)
+                print(result_handle.getvalue())
+                bestand.write(result_handle.getvalue())
+                bestand.close()
+
+                result_handle = open("Resultaat.xml", "r")
+                blast_records = NCBIXML.parse(result_handle)
+                blast_record = next(blast_records)
+                for alignment in blast_record.alignments:
+                    for hsp in alignment.hsps:
+                        blast_resultaat.append("****Alignment****")
+                        titel = alignment.title
+                        titels = titel.split("|")
+                        titel = "Blast organisme: " + titels[4]
+                        blast_resultaat.append("Blast organisme: {}".format(titels[4]))
+                        blast_resultaat.append("Sequence: {}".format(alignment.title))
+                        blast_resultaat.append("Length: {}".format(alignment.length))
+                        blast_resultaat.append("E-value: {}".format(hsp.expect))
+                        blast_resultaat.append(hsp.query[0:75] + "...")
+                        blast_resultaat.append(hsp.match[0:75] + "...")
+                        blast_resultaat.append(hsp.sbjct[0:75] + "...")
+                        blast_resultaat.append("\n")
+            if titel == '':
                 titel = "Er is geen match gevonden met de dna sequentie: {}".format(sequentie)
-        if eiwit is True:
-            result_handle = NCBIWWW.qblast("blastx", "nr", str(sequentie))
-            bestand = open("Resultaat_blastx.xml", "w")
-            resultaat_xml = result_handle.readlines()
-            bestand.writelines(resultaat_xml)
-            bestand.close()
-
-            result_handle = open("Resultaat_blastx.xml", "r")
-            blast_records = NCBIXML.parse(result_handle)
-            blast_record = next(blast_records)
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    titel = alignment.title
-                    titels = titel.split("[")
-                    titelss = titels[1].split("]")
-                    titel = "Blast organisme: " + titelss[0]
-                    break
-            if eiwit is True and titel == '':
-                titel = "Er is geen match gevonden met de eiwit sequentie: {}".format(sequentie)
-
+        titel = "\n".join(blast_resultaat)
         return render_template('BLAST.html', sequentie=sequentie, resultaat=resultaat, titel=titel)
 
 
